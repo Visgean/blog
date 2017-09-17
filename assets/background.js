b09674bccb0f4be8fa0d99198232776f961f05a7
Visgean Skeloru <me@visgean.me>
2017-09-17 22:34:43 +0100

    am retard. please fix.

--- assets/background.js
+++ assets/background.js
@@ -13,8 +13,8 @@ function changeBackground() {
         seed: getSeed(),
         x_colors: 'Spectral',
         y_colors: 'random',
-        width: min(3000, window.innerWidth),
-        height: min(6000, $(document).height() + 100)
+        width: Math.min(3000, window.innerWidth),
+        height: Math.min(6000, $(document).height() + 100)
     });
 
     // umm yeah, hacks, who does not love them?!
